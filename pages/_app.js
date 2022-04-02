f7ad90e26ffabf8ed0735218e29124dd0242a1e9
Hannah Obote <ao223ir@student.lnu.se>
2022-04-02 17:22:39 +0200

    remove merge conflic

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-<<<<<<< HEAD
-  
-=======
->>>>>>> c7a08e9055a58ed4097364e2f63e871d36f06079
   return <Component {...pageProps} />
 }
 
